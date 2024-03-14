c145c00eda2994ceff2c7f22406d89a85b76af41
R2bEEaton <scgryan@gmail.com>
2024-03-13 21:34:37 -0400

    finishing up yay

--- src/main/java/cc/spea/vaultblockplugin/VaultBlockPlugin.java
+++ src/main/java/cc/spea/vaultblockplugin/VaultBlockPlugin.java
@@ -1,16 +1,25 @@
 package cc.spea.vaultblockplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootContext;
+import org.bukkit.loot.LootTable;
+import org.bukkit.loot.LootTables;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class VaultBlockPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        getCommand("vbp").setExecutor(new CommandManager(this));
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -22,8 +31,23 @@ public class VaultBlockPlugin extends JavaPlugin implements Listener {
         if (event.getItem() == null) return;
         if (event.getItem().getType() != Material.TRIAL_KEY) return;
 
-        if (event.getClickedBlock().hasMetadata("vaultblockplugin")) {
-            Bukkit.broadcastMessage("test lol");
+        Random random = new Random();
+        event.getClickedBlock().getWorld().playSound(event.getClickedBlock().getLocation(), Sound.ENTITY_TURTLE_EGG_CRACK, 1.0f, 0.5f);
+        LootContext.Builder lootContextBuilder = new LootContext.Builder(event.getClickedBlock().getLocation());
+
+        // Pick between ship treasure and trial reward
+        List<ItemStack> lootItems = new ArrayList<>();
+        if (random.nextBoolean()) {
+            lootItems.addAll(LootTables.TRIAL_CHAMBERS_REWARD.getLootTable().populateLoot(random, lootContextBuilder.build()));
+        } else {
+            lootItems.addAll(LootTables.SHIPWRECK_TREASURE.getLootTable().populateLoot(random, lootContextBuilder.build()));
         }
+
+        // Drop all items
+        for (ItemStack item : lootItems) {
+            event.getClickedBlock().getWorld().dropItemNaturally(event.getClickedBlock().getLocation(), item);
+        }
+
+        if (event.getPlayer().getGameMode() != GameMode.CREATIVE) event.getItem().setAmount(event.getItem().getAmount() - 1);
     }
 }
